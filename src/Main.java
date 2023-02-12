299db8331daa2dfae06d974079cc9361d2db16b2
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-02-12 15:57:43 +0500

    метод приветствия и получения числа из консоли

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+    private final static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(greeding());
+        System.out.println(getNumberTerminal());
+    }
+
+    private static String greeding(){
+        return "HI, IS A PROGRAM FOR CALCULATING THE NTH TRIANGULAR NUMBER";
+    }
+    private static int getNumberTerminal(){
+        System.out.print("Enter number: ");
+        return scan.nextInt();
     }
 }
