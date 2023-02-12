1a35e7b2e6e59b8ae01290d9535ac0cb3cbe1d79
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-02-12 16:14:12 +0500

    метод чтения из файла и сам файл с числом

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     private final static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
-        System.out.println(greeding());
-        System.out.println(getNumberTerminal());
+    public static void main(String[] args) throws IOException {
+        /*System.out.println(greeding());
+        System.out.println(getNumberTerminal());*/
+
+
     }
 
+    private static String getNumberFromFile(String fileName) throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        try {
+            String currentLine = bufferedReader.readLine();
+            return currentLine;
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return "LINE NOT FOUND";
+    }
     private static String greeding(){
         return "HI, IS A PROGRAM FOR CALCULATING THE NTH TRIANGULAR NUMBER";
     }
